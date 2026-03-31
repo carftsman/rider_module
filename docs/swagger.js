3d8931278d2db113460e7600d13c0518cf1aa3f1
root <root@ip-10-0-1-86.ec2.internal>
2026-03-31 11:42:24 +0000

    pushing the changes done in deployment for the perfect production code

--- docs/swagger.js
+++ docs/swagger.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const swaggerUi = require("swagger-ui-express");
 const swaggerJSDoc = require("swagger-jsdoc");
  
+const PORT = process.env.PORT || 4000;
 const options = {
   definition: {
     openapi: "3.0.0",
@@ -9,7 +11,6 @@ const options = {
       version: "1.0.0",
       description: "Rider Authentication + Registration + KYC APIs",
     },
- 
     components: {
       securitySchemes: {
         bearerAuth: {
@@ -19,7 +20,6 @@ const options = {
         },
       },
     },
- 
     servers: [
       {
         url:process.env.NODE_ENV == "production"? `${process.env.RENDER_URL}` :`http://localhost:${process.env.PORT}`,
@@ -27,16 +27,31 @@ const options = {
       },
     ],
   },
- 
   apis: ["./routes/*.js"],
 };
- 
- 
+
 const swaggerSpec = swaggerJSDoc(options);
- 
+//const swaggerSetup = (app) => {
+//  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+//  console.log(`Swagger Documentation Loaded →` , process.env.NODE_ENV == "production" ?`${process.env.RENDER_URL}/api-docs`:`http://localhost:${process.env.PORT}/api-docs`);
+//};
 const swaggerSetup = (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log(`Swagger Documentation Loaded →` , process.env.NODE_ENV == "production" ?`${process.env.RENDER_URL}/api-docs`:`http://localhost:${process.env.PORT}/api-docs`);
-};
+  app.use("/api-docs", swaggerUi.serve);
  
+  app.get(
+    "/api-docs",
+    swaggerUi.setup(swaggerSpec, {
+      explorer: true,
+      swaggerOptions: {
+        url: "/api-docs/swagger.json",
+      },
+    })
+  );
+ 
+  app.get("/api-docs/swagger.json", (req, res) => {
+    res.json(swaggerSpec);
+  });
+ 
+  console.log(`Swagger → http://localhost:${PORT}/api-docs`);
+};
 module.exports = { swaggerSetup };
